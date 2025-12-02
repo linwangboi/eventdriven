b99ca83c15987e9665a5bf05f51ae09a41e9bcdb
linwangboi <linwangboi@gmail.com>
2025-12-02 11:40:01 +0000

    .

---
+++ react-events/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="py-5">
+      <div className='d-grid gap-2 d-sm-flex justify-center-sm-center mb-5'>
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
